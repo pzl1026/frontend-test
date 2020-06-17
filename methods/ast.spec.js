16912b729ca7d3d3ea1b6a6b332bf18cfcc4137f
pzl <pzl1026@aliyun.com>
2020-06-17 18:01:58 +0800

    提交

---
+++ methods/ast.spec.js
@@ -0,0 +1,14 @@
+const { Parser, DomHandler } = require('htmlparser2');
+const util = require('util');
+let handler = new DomHandler(function (err, dom) {
+  console.log(util.inspect(dom, { showHidden: false, depth: null }));
+});
+const parser = new Parser(handler, {
+  recognizeCDATA: true,
+});
+
+describe('myReduce test', function () {
+  it('should myReduce', function () {
+    parser.parseComplete('<div id="111"><span>19238123</span></div>');
+  });
+});
