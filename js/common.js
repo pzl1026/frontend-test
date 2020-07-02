b70df2f85c34af82b73eec6ca0977a668f5dfec2
pzl <pzl1026@aliyun.com>
2020-07-02 22:54:50 +0800

    t提交

--- js/common.js
+++ js/common.js
@@ -216,3 +216,9 @@ const flatten = function (arr) {
 let arr = [1, 2, [3, 4, 5, [6, 7], 8], 9, 10, [11, [12, 13]]];
 
 console.log(flatten(arr));
+
+// 可避免setInterval因执行时间导致的间隔执行时间不一致
+setTimeout(function () {
+  // do something
+  setTimeout(arguments.callee, 500);
+}, 500);
